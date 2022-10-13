595e43e0c39cb9bf1ff0fa151b9e6bd70ca6dec4
felixjordan <felix.jordan@web.de>
2022-10-13 08:19:18 +0200

    Finished version of the game. Loop guessing and checking letters until max number of errors is reached or the word is found.

--- Python/hangman/Hangman.py
+++ Python/hangman/Hangman.py
@@ -2,7 +2,7 @@ from random_word import RandomWords
 
 
 class Hangman:
-    __MAX_NUM_FAILED = 12
+    __MAX_NUM_FAILED = 10
 
     def __init__(self):
         self.word = ''
@@ -11,10 +11,7 @@ class Hangman:
         self.guess = ''
         self.prev_guesses = []
 
-    def print_word(self):
-        print(self.word)
-
-    def initialize_known_letters(self):
+    def init_known_letters_as_underscores(self):
         self.known_letters = ''.join(['_' for c in self.word])
 
     def choose_word(self):
@@ -27,16 +24,13 @@ class Hangman:
             guess = input('Choose a letter:').lower()
             if guess.isalpha() & (len(guess) == 1):
                 if guess not in self.prev_guesses:
+                    self.prev_guesses.append(guess)
                     self.guess = guess
                     break
                 else:
-                    print('This letter has already been guessed. All previously guessed letters are:',
-                          self.prev_guesses)
+                    print('This letter has already been guessed. Go again.')
             else:
                 print('Invalid input. Choose a single letter.')
-
-    def find_matches(self):
-        return [i for i, letter in enumerate(self.word) if letter == self.guess]
 
     def update_known_letters(self, matches):
         kl = [letter for letter in self.known_letters]
@@ -45,22 +39,32 @@ class Hangman:
         self.known_letters = ''.join(kl)
 
     def check_guess(self):
-        matches = self.find_matches()
+        matches = [i for i, letter in enumerate(self.word) if letter == self.guess]
         if len(matches) == 0:
             self.num_failed += 1
-            print('The chosen letter does not occur in the word. Error count:',
+            print('No match.')
+            print('Error count:',
                   self.num_failed, '/', self.__MAX_NUM_FAILED)
         else:
             self.update_known_letters(matches)
             print('Nice.')
-            print(self.known_letters)
 
     def play(self):
         self.choose_word()
-        self.initialize_known_letters()
+        self.init_known_letters_as_underscores()
         print('Can you find the chosen word?')
         print(self.known_letters)
         print(self.word)
 
-        self.guess_letter()
-        self.check_guess()
+        while True:
+            self.guess_letter()
+            self.check_guess()
+            print(self.known_letters)
+            if self.num_failed == self.__MAX_NUM_FAILED:
+                print('You lost this time.')
+                print('The word was:', self.word)
+                break
+            elif self.known_letters == self.word:
+                print('You win!')
+                break
+            print('Previous guesses:', ', '.join(self.prev_guesses))
